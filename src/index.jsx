51d6ad5174206c2cff8a56577eacb22de4265ba7
Mariya Kochanova <balalum@gmail.com>
2025-02-25 12:46:43 +0100

    fixed publication errors

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/flags-of-the-coutries">
       <App />
     </BrowserRouter>
   </StrictMode>,
